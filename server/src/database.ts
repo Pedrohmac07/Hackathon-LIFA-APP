fe8dc076858b2700a6c71931ead44cc0b2fc7908
Pedro Machado <ph374011@gmail.com>
2025-11-29 19:24:14 -0300

    Updated .gitignore

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,13 +1,22 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 
+const caPath = path.resolve(process.cwd(), 'ca.pem');
+
 export const pool = mysql.createPool({
- host: process.env.DB_HOST || 'localhost',
- user: process.env.DB_USER || 'root',
+ host: process.env.DB_HOST,
+ user: process.env.DB_USER,
  password: process.env.DB_PASSWORD,
- database: process.env.DB_NAME || 'lifa_db',
+ database: process.env.DB_NAME,
+ port: Number(process.env.PORT),
+ ssl: {
+  ca: fs.readFileSync(caPath),
+  rejectUnauthorized: true
+ },
  waitForConnections: true,
  connectionLimit: 10,
  queueLimit: 0
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-// Importa o arquivo index.ts da pasta routes
 import { router } from './routes';
 
 dotenv.config();
@@ -16,6 +15,6 @@ app.use(express.json());
 app.use(router);
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🔥 Servidor LIFA rodando na porta ${PORT}`);
-  console.log(`📱 Conecte o App no IP da sua máquina (verifique com 'ip addr')`);
+ console.log(`🔥 Servidor LIFA rodando na porta ${PORT}`);
+ console.log(`📱 Conecte o App no IP da sua máquina (verifique com 'ip addr')`);
 });
